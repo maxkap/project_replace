cd6574ca2c65c6801a2eafd62b070e4867886c86
maxkap <maxkap2005@gmail.com>
2020-07-09 12:53:37 +0300

    Final version 1

--- upload/admin/controller/module/products_replace.php
+++ upload/admin/controller/module/products_replace.php
@@ -1,23 +1,19 @@
 <?php
 
-class ControllerModuleProductsReplace extends Controller {
-
-    private $error = array();
+class ControllerModuleProductsReplace extends Controller {    
 
     public function index() {
-        
+
         $this->load->language('module/products_replace');
-        
 
         $this->document->setTitle($this->language->get('heading_title'));
 
-        $this->load->model('module/products_replace');
         $this->load->model('catalog/category');
-        
+
         $filters['sort'] = 'name';
-        $filters['start'] = 0;        
-        $filters['limit'] = 1000;        
-        $data['categories']  = $this->model_catalog_category->getCategories($filters);
+        $filters['start'] = 0;
+        $filters['limit'] = 1000;
+        $data['categories'] = $this->model_catalog_category->getCategories($filters);
 
         $data['breadcrumbs'] = array();
 
@@ -28,45 +24,64 @@ class ControllerModuleProductsReplace extends Controller {
 
         $data['breadcrumbs'][] = array(
             'text' => $this->language->get('heading_title'),
-            'href' => $this->url->link('module/products_replace', 'token=' . $this->session->data['token'] , true)
+            'href' => $this->url->link('module/products_replace', 'token=' . $this->session->data['token'], true)
         );
 
         $data['start'] = $this->url->link('module/products_replace/start', 'token=' . $this->session->data['token'], true);
         $data['token'] = $this->session->data['token'];
-
-
 
         $data['heading_title'] = $this->language->get('heading_title');
         $data['text_success'] = $this->language->get('text_success');
         $data['text_fail'] = $this->language->get('text_fail');
         $data['text_categories_from'] = $this->language->get('text_categories_from');
         $data['text_category_to'] = $this->language->get('text_category_to');
-        
+
         $data['text_start'] = $this->language->get('text_start');
         $data['text_replace_to'] = $this->language->get('text_replace_to');
         $data['text_replace_to_desc'] = $this->language->get('text_replace_to_desc');
         $data['text_replace_from'] = $this->language->get('text_replace_from');
         $data['text_replace_from_desc'] = $this->language->get('text_replace_from_desc');
         $data['text_warning'] = $this->language->get('text_warning');
-        $data['is_fail'] = $data['is_success'] = false;
-        
-        
 
+        if (isset($this->session->data['is_fail'])) {
+            $data['is_fail'] = $this->session->data['is_fail'];
+            unset($this->session->data['is_fail']);
+        } else {
+            $data['is_fail'] = '';
+        }
+        if (isset($this->session->data['is_success'])) {
+            $data['is_success'] = $this->session->data['is_success'];
+            unset($this->session->data['is_success']);
+        } else {
+            $data['is_success'] = '';
+        }
 
         $data['header'] = $this->load->controller('common/header');
         $data['column_left'] = $this->load->controller('common/column_left');
         $data['footer'] = $this->load->controller('common/footer');
-        
-        
 
         $this->response->setOutput($this->load->view('module/products_replace', $data));
     }
-    
-    
+
     public function start() {
-        
-        var_dump($this->request->post);
-        
+        $this->load->language('module/products_replace');
+        $this->load->model('module/products_replace');
+
+        if (isset($this->request->post['categories']) && isset($this->request->post['to_category_id'])) {
+
+            $category_to = (int) $this->request->post['to_category_id'];
+            $categories = $this->request->post['categories'];
+
+            $replace_to = (isset($this->request->post['replace_to'])) ? 1 : 0;
+            $replace_from = (isset($this->request->post['replace_from'])) ? 1 : 0;
+
+            $this->model_module_products_replace->startReplace($categories, $category_to, $replace_to, $replace_from);
+
+            $this->session->data['is_success'] = $this->language->get('text_success');
+        } else {
+            $this->session->data['is_fail'] = $this->language->get('text_fail');
+        }
+        $this->response->redirect($this->url->link('module/products_replace', 'token=' . $this->session->data['token'], true));
     }
 
 }
--- upload/admin/model/module/products_replace.php
+++ upload/admin/model/module/products_replace.php
@@ -1,4 +1,64 @@
 <?php
 class ModelModuleProductsReplace extends Model {
     
+    public function getProductIdsByCategory($category_id) {
+        
+        $product_ids = array();
+
+        $query = $this->db->query("SELECT product_id FROM " . DB_PREFIX . "product_to_category WHERE category_id = '" . (int)$category_id . "'");
+
+        foreach ($query->rows as $result) {
+                $product_ids[] = $result['product_id'] ;
+        }
+
+        return $product_ids;
+        
+    }
+    
+    
+    public function deleteProductsFromCategory($category_id) {
+        $this->db->query("DELETE FROM " . DB_PREFIX . "product_to_category WHERE category_id = '" . (int)$category_id . "'");
+    }
+    
+    public function startReplace($categories_from, $category_to, $replace_to = 0, $replace_from = 0) {
+        
+        if ($replace_to){
+            $this->deleteProductsFromCategory($category_to);
+        }
+        
+        $products_to_insert = array();
+        
+        foreach ($categories_from as $category_id) {
+            
+            $products = $this->getProductIdsByCategory($category_id);
+            if ($products)  {
+                $products_to_insert = array_merge($products_to_insert, $products);
+            }
+            if ($replace_from){
+                $this->deleteProductsFromCategory($category_id);
+            }
+            
+        }
+        
+        
+        $this->insertProductsToCategory($products_to_insert, $category_to);
+    }
+    
+    
+    public function insertProductsToCategory($products_to_insert, $category_id) {
+        
+        $sql = "REPLACE INTO `" . DB_PREFIX . "product_to_category` (`product_id`, `category_id`) VALUES ";
+        
+        $insert = array();
+        
+        foreach ($products_to_insert as $product_id) {
+            
+            $insert[] = " ($product_id, $category_id) ";
+            
+        }
+        
+        $this->db->query($sql . implode(',', $insert));
+        
+    }
+    
 }
